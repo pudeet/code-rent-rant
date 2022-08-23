f9b5b651545e8a4528dae8b2a68a39c1828d521d
pudeet <henryotto35@gmail.com>
2022-08-23 00:08:40 -0400

    fixed an issue

--- controllers/places.js
+++ controllers/places.js
@@ -4,22 +4,6 @@ router.get('/', (req, res) => {
     res.send('GET /places')
 })
 
-// More code here in a moment
-
 module.exports = router
 
-require('dotenv').config()
-const express = require('express')
-const app = express()
-
-app.use('/places', require('./controllers/places'))
-
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
-
-app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
-})
-
-app.listen(process.env.PORT)
+// More code here in a moment
